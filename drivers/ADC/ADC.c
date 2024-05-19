dfac55893576fa19b1b0d4d046377b4175e972a4
Paula Ayman Mikhael Boktur <85376839+PaulAyman@users.noreply.github.com>
2024-05-20 00:26:31 +0300

    Add files via upload

--- drivers/ADC/ADC.c
+++ drivers/ADC/ADC.c
@@ -12,10 +12,12 @@ void ADC_Init(void){
 	GPIO_PORTE_AFSEL_R |= (1<<1); // ENABLE ALTERNATIVE FUNC FOR PE1
 	GPIO_PORTE_DEN_R  &= ~(1<<1); // DISABLE DIG FUNC FOR PE1
 	GPIO_PORTE_AMSEL_R |= (1<<1); // ENABLE ANALOG FUNC FOR PE1
+
 	ADC1_ACTSS_R &= ~(1<<3); // DISABLE SAMPLE SEQ
 	ADC1_EMUX_R = (0xF << 12); // TRIGGER: ALWAYS SAMPLING
 	ADC1_SSMUX3_R = 2; // READ SEQ3 FROM AIN2, SHOK
 	ADC1_SSCTL3_R |= (1<<1) | (1<<2); // End bit set, interrupt bit enabled for ss3
+	
 	ADC1_IM_R |= (1<<3); // enable int mask for ss3
 	ADC1_RIS_R  |= (1<<3); // clear int status for ss3 
 	ADC1_ACTSS_R |= (1<<3); // ENABLE SAMPLE SEQ
